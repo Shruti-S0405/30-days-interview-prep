bc8f45928ed0ca691e09148319b450cf915bbd91
shruti-s0405 <shruti.sundar0405@gmail.com>
2024-12-06 17:30:17 +0530

    Day 3 interview prep -operators

---
+++ day3/operators.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+
+
+public class operators {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+}
+class Result {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+    // Write your code here
+        double total = ((tip_percent+tax_percent)*0.01*meal_cost)+meal_cost;
+        System.out.println(Math.round(total));
+    }
+
+}
---
+++ day3/readme.md
@@ -0,0 +1,10 @@
+#30 Days of Interview Preparation
+
+##Day 3
+
+###Challenge - 1
+
+https://www.hackerrank.com/challenges/30-operators/problem
+
+####Operators
+
